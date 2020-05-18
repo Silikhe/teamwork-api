1fc089ca198bb435654e3993a1c5117b00d490e8
Sil silas <silikhesilas@gmail.address>
2020-05-18 23:42:20 +0300

    Update

--- index.js
+++ index.js
@@ -36,3 +36,50 @@ app.get('/api/v1', (req, res) => {
 		}
 	});
 });
+
+
+
+
+
+
+
+
+
+const express = require('express');
+require('dotenv').config();
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//request from cross origin
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+	);
+	next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//port
+// process.env.NODE_ENV = process.env.PORT;
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`server app listening at:${port}`));
+
+//api versionig
+app.get('/api/v1', (req, res) => {
+	res.json({
+		status: 'success',
+		data: {
+			message: 'hello world'
+		}
+	});
+});
